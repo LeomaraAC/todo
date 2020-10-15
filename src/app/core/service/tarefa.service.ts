3494055862512921dc0e6bc196b9af5da7517069
Leomara <leomara.castro@apagri.com.br>
2020-10-14 22:12:45 -0300

    Atualizando o status da tarefa

--- src/app/core/service/tarefa.service.ts
+++ src/app/core/service/tarefa.service.ts
@@ -1,9 +1,20 @@
 import { Injectable } from '@angular/core';
+import {Tarefa} from '../../model/tarefa.model';
+import {api} from '../utils/global.constants';
+import {HttpClient} from '@angular/common/http';
+import {Observable} from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class TarefaService {
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
+
+  /**
+   * Atualiza os dados de uma tarefa
+   */
+  atualizar(tarefa: Tarefa): Observable<Tarefa> {
+    return this.http.put<Tarefa>(`${api}/tarefas/${tarefa.id}`, tarefa);
+  }
 }
--- src/app/page/tarefa/tarefa.page.ts
+++ src/app/page/tarefa/tarefa.page.ts
@@ -3,6 +3,7 @@ import {CategoriaService} from '../../core/service/categoria.service';
 import {Categoria} from '../../model/categoria.model';
 import {Tarefa} from '../../model/tarefa.model';
 import {Subscription} from 'rxjs';
+import {TarefaService} from '../../core/service/tarefa.service';
 
 @Component({
   selector: 'app-tarefa',
@@ -13,7 +14,8 @@ export class TarefaPage implements OnInit {
   categorias: Categoria[] = [];
   subs: Subscription[] = [];
 
-  constructor(private categoriaService: CategoriaService) { }
+  constructor(private categoriaService: CategoriaService,
+              private tarefaService: TarefaService) { }
 
   ngOnInit() {
     this.buscarCategoriasTarefas();
@@ -38,4 +40,16 @@ export class TarefaPage implements OnInit {
             .subscribe(categorias => this.categorias = categorias)
     );
   }
+
+  concluirTarefa(tarefa: Tarefa) {
+    tarefa.concluido = !tarefa.concluido;
+    this.subs.push(
+        this.tarefaService.atualizar(tarefa).subscribe(data => {
+          console.log('data', data);
+        }, err => {
+          console.log(err);
+        })
+    );
+  }
+
 }
