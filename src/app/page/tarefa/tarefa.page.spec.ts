6a2567d7dcd20f5ec0e36523cb2d2493c1fbbbb0
Leomara <leomara.castro@apagri.com.br>
2020-12-16 23:37:39 -0300

    Casos de testes para excluir tarefas

--- src/app/page/tarefa/tarefa.page.spec.ts
+++ src/app/page/tarefa/tarefa.page.spec.ts
@@ -1,5 +1,5 @@
 import {async, ComponentFixture, fakeAsync, TestBed, tick} from '@angular/core/testing';
-import {IonicModule} from '@ionic/angular';
+import {AlertController, IonicModule} from '@ionic/angular';
 
 import {TarefaPage} from './tarefa.page';
 import {TarefaService} from '../../core/service/tarefa.service';
@@ -7,7 +7,10 @@ import {CategoriaService} from '../../core/service/categoria.service';
 import {Router} from '@angular/router';
 import {RouterTestingModule} from '@angular/router/testing';
 import {Categoria} from '../../model/categoria.model';
-import {of} from 'rxjs';
+import {EMPTY, of} from 'rxjs';
+
+import * as _ from 'lodash';
+import {alertReturn} from '../../core/tests/mocks';
 
 describe('TarefaPage', () => {
   let component: TarefaPage;
@@ -15,20 +18,23 @@ describe('TarefaPage', () => {
   let router: Router;
   let tarefaSpy;
   let categoriaSpy;
+  let alertSpy;
+  const tarefasFake = [{
+    categoriaId: 1,
+    descricao: 'Tarefa 1',
+    concluido: false,
+    id: 1
+  }];
   const categoriaFake: Categoria[] = [{
     id: 1,
     titulo: 'Teste 1',
-    tarefas: [{
-      categoriaId: 1,
-      descricao: 'Tarefa 1',
-      concluido: false,
-      id: 1
-    }]
+    tarefas: tarefasFake
   }];
 
   beforeEach(async(() => {
     tarefaSpy = jasmine.createSpyObj('tarefaService', ['excluir', 'atualizar']);
     categoriaSpy = jasmine.createSpyObj('categoriaService', ['buscarTodasCategoriasTarefas']);
+    alertSpy = jasmine.createSpyObj('AlertController', ['create']);
 
     TestBed.configureTestingModule({
       declarations: [TarefaPage],
@@ -38,7 +44,8 @@ describe('TarefaPage', () => {
         )],
       providers: [
         {provide: TarefaService, useValue: tarefaSpy},
-        {provide: CategoriaService, useValue: categoriaSpy}
+        {provide: CategoriaService, useValue: categoriaSpy},
+        {provide: AlertController, useValue: alertSpy}
       ]
     }).compileComponents();
 
@@ -61,5 +68,40 @@ describe('TarefaPage', () => {
     expect(component.subs.length).toBe(2);
     expect(categoriaSpy.buscarTodasCategoriasTarefas.calls.count()).toBe(1);
   });
+
+  it('Deve exibir um alert ao clicar no botão de excluir', () => {
+    fixture.detectChanges();
+    const tarefa = tarefasFake[0];
+    const btnExcluir: HTMLElement = fixture.nativeElement.querySelector(`#excluir_${tarefa.id}`);
+    btnExcluir.click();
+    expect(alertSpy.create).toHaveBeenCalled();
+  });
+
+  it('Não deve possuir ação no botão de cancelar', fakeAsync(() => {
+    alertSpy.create.and.returnValue(alertReturn);
+
+    component.excluir(tarefasFake[0]);
+    tick(); // Tempo necessário para chamar o método present do alert
+    const {buttons} = alertSpy.create.calls.first().args[0];
+    const cancelButton = buttons[0];
+
+    expect(_.has(cancelButton, 'handler')).toBeFalse();
+    expect(tarefaSpy.excluir).not.toHaveBeenCalled();
+  }));
+
+  it('Deve remover o item ao confirmar o alert', fakeAsync(() => {
+    alertSpy.create.and.returnValue(alertReturn);
+    tarefaSpy.excluir.and.returnValue(EMPTY);
+    const tarefa = tarefasFake[0];
+
+    component.excluir(tarefa);
+    tick(); // Tempo necessário para chamar o método present do alert
+    const {buttons} = alertSpy.create.calls.first().args[0];
+    const confirmButton = buttons[1];
+    confirmButton.handler();
+
+    expect(tarefaSpy.excluir).toHaveBeenCalledWith(tarefa);
+    expect(categoriaSpy.buscarTodasCategoriasTarefas).toHaveBeenCalled();
+  }));
 
 });
